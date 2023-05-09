a37cc30fd43202fedcb6df937482b11c8bb32a75
kalibetre <kalbetre@icloud.com>
2023-05-10 00:38:52 +0300

    rename to jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App/App';
+import SignUp from './components/Auth/SignUp';
 import './css-reset.css';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        {/* <App /> */}
+        <SignUp />
     </React.StrictMode>
 );
