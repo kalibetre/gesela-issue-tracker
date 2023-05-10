eede4725ddd0387cc462d0a44c7464f094afd6ad
kalibetre <kalbetre@icloud.com>
2023-05-10 14:17:48 +0300

    add SideBar component

---
+++ src/components/SideBar/SideBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Logo from '../Logo/Logo'
+import './SideBar.css'
+
+const SideBar = (props) => {
+	return (
+		<div className='sdb-container'>
+			<div className='sdb-header'>
+				<div className='sdb-logo'>
+					<Logo />
+				</div>
+			</div>
+			<div className="sdb-menu">
+				{props.children}
+			</div>
+		</div>
+	)
+}
+
+export default SideBar
