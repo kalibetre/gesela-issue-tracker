8ca29c57deb2c484f40749db8c561513b7e389c8
kalibetre <kalbetre@icloud.com>
2023-05-10 14:18:24 +0300

    setup main page layout

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,26 @@
+import Header from '../Header/Header';
+import SideBar from '../SideBar/SideBar';
 import './App.css';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <h1>Gesela Issue Tracker</h1>
-            </header>
+        <div className="ap-container">
+            <div className="ap-content">
+                <div className="ap-sidebar">
+                    <SideBar>
+                        <p>Menus</p>
+                    </SideBar>
+                </div>
+                <div className='ap-main-container'>
+                    <div className="ap-header">
+                        <Header title="All Issues">Header</Header>
+                    </div>
+                    <main className="ap-main">Issues</main>
+                </div>
+            </div>
+            <footer className="ap-footer">
+                Gesela &copy; 2023
+            </footer>
         </div>
     );
 }
