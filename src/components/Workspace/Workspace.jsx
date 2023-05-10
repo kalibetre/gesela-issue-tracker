31bf9e3a04573f5a4236e2b882527cf55e37304a
kalibetre <kalbetre@icloud.com>
2023-05-10 16:26:46 +0300

    add Workspace component

---
+++ src/components/Workspace/Workspace.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Workspace.css'
+
+const Workspace = (props) => {
+	return (
+		<section className='wks-main-container'>
+			<div className="wks-header">
+				<h2 className="wks-title">
+					{props.title}
+				</h2>
+			</div>
+			<main className="wks-main">
+				{props.title}
+			</main>
+		</section>
+	)
+}
+
+export default Workspace
