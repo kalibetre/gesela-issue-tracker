13705dc50b18ae23076cbe8411d617e5a2424822
kalibetre <kalbetre@icloud.com>
2023-05-12 13:16:59 +0300

    adjust modal style

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,25 +1,26 @@
-import React from 'react'
-import { createPortal } from 'react-dom'
-import './Modal.css'
+import React from 'react';
+import { createPortal } from 'react-dom';
+import './Modal.css';
 
 const Modal = (props) => {
+    return createPortal(
+        <div className="modal-overlay">
+            <div className="modal-container">
+                <div className="modal-header">
+                    <h3 className="modal-title">{props.title}</h3>
+                    <button
+                        className="modal-close-btn"
+                        onClick={props.handleClose}
+                    >
+                        &times;
+                    </button>
+                </div>
+                <div className="modal-body">{props.children}</div>
+                <div className="modal-footer">{props.actions}</div>
+            </div>
+        </div>,
+        document.body
+    );
+};
 
-	return createPortal(
-		<div className='modal-overlay'>
-			<div className='modal-container'>
-				<div className='modal-header'>
-					<h3 className='modal-title'>{props.title}</h3>
-					<button className='modal-close-btn' onClick={props.handleClose}>&times;</button>
-				</div>
-				<div className='modal-body'>
-						{props.children}
-				</div>
-				<div className="modal-footer">
-					{props.actions}
-				</div>
-			</div>
-		</div>, document.body
-	)
-}
-
-export default Modal
+export default Modal;
