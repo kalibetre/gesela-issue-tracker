cfc4fc4f6acc492d96e30ef5f70ad55ba7335920
kalibetre <kalbetre@icloud.com>
2023-05-12 14:46:51 +0300

    add utility functions for groupBy and filterBy

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,19 @@
 export const truncateText = (str, n) => {
     return str?.length > n ? str.substr(0, n - 1) + '...' : str;
 };
+
+export const groupByAttribute = (data, attributeName) => {
+    const groups = {};
+    for (const issue of data) {
+        const attributeValue = issue[attributeName];
+        if (!groups[attributeValue]) {
+            groups[attributeValue] = [];
+        }
+        groups[attributeValue].push(issue);
+    }
+    return groups;
+};
+
+export const filterByAttribute = (data, attribute, value) => {
+    return data.filter((issue) => issue[attribute] === value);
+};
