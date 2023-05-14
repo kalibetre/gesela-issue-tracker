73250406a6056b9e98db293170da10b6fa5502ba
kalibetre <kalbetre@icloud.com>
2023-05-14 12:40:58 +0300

    query user profile

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,17 +1,21 @@
 import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { useGetProfileQuery } from '../api/userApi';
+import LoadingPage from '../components/LoadingPage/LoadingPage';
 
 const useAuth = () => {
-    const { currentUser, loading } = useSelector((state) => state.auth);
+    const { data: currentUser, isLoading, isError } = useGetProfileQuery();
     const navigate = useNavigate();
 
     useEffect(() => {
-        if (!currentUser) {
+        if (!isLoading && !currentUser) {
             navigate('/signin');
         }
-    }, [currentUser, navigate]);
-    return { currentUser, loading };
+    }, [currentUser, isLoading, navigate]);
+
+    if (isLoading) return <LoadingPage />;
+
+    return { currentUser, isLoading, isError };
 };
 
 export default useAuth;
