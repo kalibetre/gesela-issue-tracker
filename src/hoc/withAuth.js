c2b2a964161791fc691e904ba571478df3df2847
kalibetre <kalbetre@icloud.com>
2023-05-14 13:40:25 +0300

    bug fix for stale token

--- src/hoc/withAuth.js
+++ src/hoc/withAuth.js
@@ -1,20 +1,38 @@
 import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { useGetProfileQuery } from '../api/userApi';
 import LoadingPage from '../components/LoadingPage/LoadingPage';
+import { logout } from '../store/authSlice';
 
 const withAuth = (WrappedComponent) => {
     return (props) => {
-        const { data: currentUser, isLoading, isError } = useGetProfileQuery();
+        const {
+            data: currentUser,
+            isLoading: profileLoading,
+            isError,
+        } = useGetProfileQuery();
+        const { token, isLoading: tokenLoading } = useSelector(
+            (state) => state.auth
+        );
         const navigate = useNavigate();
+        const dispatch = useDispatch();
 
         useEffect(() => {
-            if (!isLoading && !currentUser) {
+            if (!profileLoading && !tokenLoading && !currentUser) {
+                if (token) dispatch(logout());
                 navigate('/signin');
             }
-        }, [currentUser, isLoading, navigate]);
+        }, [
+            currentUser,
+            profileLoading,
+            tokenLoading,
+            navigate,
+            token,
+            dispatch,
+        ]);
 
-        if (isLoading) return <LoadingPage />;
+        if (profileLoading || tokenLoading) return <LoadingPage />;
         if (isError) return <div>Something went wrong...</div>;
 
         return <WrappedComponent {...props} />;
