2b0cbd5830b3d8cb5d16dd741da15686b2410e2b
kalibetre <kalbetre@icloud.com>
2023-05-15 12:34:00 +0300

    add departments component

--- src/components/Departments/Departments.jsx
+++ src/components/Departments/Departments.jsx
@@ -1,19 +1,43 @@
 import React from 'react';
 import { useGetDepartmentsQuery } from '../../api/departmentApi';
+import '../Common/Cards.css';
 import Department from '../Department/Department';
-import LoadingPage from '../LoadingPage/LoadingPage';
+import StatusMessage from '../StatusMessage/StatusMessage';
 import Workspace from '../Workspace/Workspace';
+import './Departments.css';
 
 const Departments = () => {
-    const { data: departments, isLoading } = useGetDepartmentsQuery();
+    const { data: departments, isLoading, isError } = useGetDepartmentsQuery();
 
-    if (isLoading) return <LoadingPage />;
+    const handleOnClick = (department) => {
+        console.log(department);
+    };
+
+    const handleNewDepartment = () => {
+        console.log('New Department');
+    };
 
     return (
         <Workspace title="Departments">
-            {departments.map((department) => (
-                <Department key={department.id} department={department} />
-            ))}
+            {isLoading ? (
+                <StatusMessage loading title="Loading departments .." />
+            ) : isError ? (
+                <StatusMessage error title="Error departments .." />
+            ) : (
+                <div className="card-list-container">
+                    <div className="card-list">
+                        {departments &&
+                            departments.map((department) => (
+                                <Department
+                                    key={department.uuid}
+                                    department={department}
+                                    onClick={() => handleOnClick(department)}
+                                />
+                            ))}
+                        <Department actionCard onClick={handleNewDepartment} />
+                    </div>
+                </div>
+            )}
         </Workspace>
     );
 };
