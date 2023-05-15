4d4e25e46adf75306f4b0ae58b687406ab16cfb5
kalibetre <kalbetre@icloud.com>
2023-05-15 12:34:15 +0300

    add department card

--- src/components/Department/Department.jsx
+++ src/components/Department/Department.jsx
@@ -1,8 +1,41 @@
 import React from 'react';
+import { truncateText } from '../../utils/utils';
+import '../Common/Cards.css';
+import { PlusIcon } from '../Common/Icons';
+import './Department.css';
 
 const Department = (props) => {
     const { department } = props;
-    return <div>{department.name}</div>;
+
+    return (
+        <div className="dept-card" onClick={props.onClick}>
+            <div className="card">
+                {props.actionCard ? (
+                    <div className="action-card">
+                        <div className="action-card-icon">
+                            <PlusIcon />
+                        </div>
+                        <div className="action-card-text">
+                            Add New Department
+                        </div>
+                    </div>
+                ) : (
+                    <>
+                        <header className="card-header">
+                            <h3 className="card-header-title">
+                                {department.name}
+                            </h3>
+                        </header>
+                        <div className="card-content">
+                            <p className="card-text">
+                                {truncateText(department.description, 100)}
+                            </p>
+                        </div>
+                    </>
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default Department;
