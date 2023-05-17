33c311b81b3f616c8908839137e3687ebdced06b
kalibetre <kalbetre@icloud.com>
2023-05-18 00:13:31 +0300

    add isManager and isSubmitted helper check functions

--- src/utils/issue.js
+++ src/utils/issue.js
@@ -2,11 +2,15 @@ export const isDraft = (issue) => issue && issue.status === 'DRAFT';
 
 export const isArchived = (issue) => issue && issue.status === 'ARCHIVED';
 
+export const isSubmitted = (issue) => issue && issue.status === 'SUBMITTED';
+
 export const isOwner = (issue, user) =>
-    issue && user && issue.raisedBy.uuid === user.uuid;
+    issue && user && issue.raisedBy?.uuid === user.uuid;
 
 export const isHandler = (issue, user) =>
     issue && user && issue.handler && issue.handler.uuid === user.uuid;
+
+export const isManager = (user) => user && user.role === 'ISSUE_MANAGER';
 
 export const ISSUE_STATUS = {
     DRAFT: 'DRAFT',
