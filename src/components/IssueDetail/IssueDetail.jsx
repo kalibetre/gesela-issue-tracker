4ea267e57c0d640a85e3c3c1c249f9c1046bd6e6
kalibetre <kalbetre@icloud.com>
2023-05-17 22:58:41 +0300

    impl role based issue functionalities

--- src/components/IssueDetail/IssueDetail.jsx
+++ src/components/IssueDetail/IssueDetail.jsx
@@ -1,10 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+    useDeleteIssueMutation,
+    useUpdateIssueMutation,
+} from '../../api/issueApi';
+import { useGetProfileQuery } from '../../api/userApi';
+import { isArchived, isDraft, isHandler, isOwner } from '../../utils/issue';
 import { getStatusCSSClass } from '../../utils/utils';
+import EditIssueModal from '../EditIssueModal/EditIssueModal';
 import Modal from '../Modal/Modal';
 import './IssueDetail.css';
 
 const IssueDetail = (props) => {
     const { issue } = props;
+    const { data: currentUser } = useGetProfileQuery();
+    const [openEdit, setOpenEdit] = useState(false);
+    const [updateIssue, { isLoading: isUpdating, isError: updateError }] =
+        useUpdateIssueMutation();
+    const [deleteIssue, { isLoading: isDeleting, isError: deleteError }] =
+        useDeleteIssueMutation();
+    const [error, setError] = useState(null);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            if (issue) {
+                await updateIssue({
+                    uuid: issue.uuid,
+                    status: 'SUBMITTED',
+                }).unwrap();
+                props.handleClose();
+            }
+        } catch (e) {
+            setError('Unable to submit the issue.');
+        }
+    };
+
+    const handleDelete = async (e) => {
+        e.preventDefault();
+        try {
+            if (issue) {
+                await deleteIssue(issue.uuid).unwrap();
+                props.handleClose();
+            }
+        } catch (e) {
+            setError('Unable to delete the issue');
+        }
+    };
 
     return (
         <Modal
@@ -15,22 +56,56 @@ const IssueDetail = (props) => {
                     <button
                         className="btn btn-default"
                         onClick={props.handleClose}
+                        disabled={isUpdating || isDeleting}
                     >
                         Close
                     </button>
-                    <button className="btn btn-danger">Delete</button>
-                    <button className="btn btn-primary">Save</button>
-                    <button className="btn btn-primary">Submit</button>
+                    {isDraft(issue) && isOwner(issue, currentUser) && (
+                        <button
+                            className="btn btn-danger"
+                            onClick={handleDelete}
+                            disabled={isUpdating || isDeleting}
+                        >
+                            Delete
+                        </button>
+                    )}
+                    {isDraft(issue) && isOwner(issue, currentUser) && (
+                        <button
+                            className="btn btn-primary"
+                            onClick={handleSubmit}
+                            disabled={isUpdating || isDeleting}
+                        >
+                            Submit
+                        </button>
+                    )}
+                    {((isOwner(issue, currentUser) && isDraft(issue)) ||
+                        isHandler(issue, currentUser)) &&
+                        !isArchived(issue) && (
+                            <button
+                                className="btn btn-primary"
+                                onClick={(e) => setOpenEdit(true)}
+                                disabled={isUpdating || isDeleting}
+                            >
+                                Edit
+                            </button>
+                        )}
                 </>
             }
         >
+            {openEdit && issue && (
+                <EditIssueModal
+                    issue={issue}
+                    handleClose={(e) => setOpenEdit(false)}
+                    disableSubmit
+                />
+            )}
             <div className="isu-detail">
                 <div
                     className={
                         'isu-detail-prog ' + getStatusCSSClass(issue.status)
                     }
                 >
-                    {issue.status}
+                    {issue.status.replace('_', ' ')}
                 </div>
                 <div className="isu-detail-row">
                     <p className="isu-detail-label">Title</p>
@@ -39,10 +114,6 @@ const IssueDetail = (props) => {
                 <div className="isu-detail-row">
                     <p className="isu-detail-label">Description</p>
                     <p className="isu-detail-desc">{issue.description}</p>
-                </div>
-                <div className="isu-detail-row">
-                    <p className="isu-detail-label">Department</p>
-                    <p className="isu-detail-desc">{issue.department}</p>
                 </div>
                 <div className="isu-detail-row">
                     <p className="isu-detail-label isu-detail-label-center">
@@ -71,6 +142,27 @@ const IssueDetail = (props) => {
                             : null}
                     </div>
                 </div>
+                {isUpdating && (
+                    <div className="isu-detail-row">
+                        <div className="alert alert-info" role="alert">
+                            Saving your changes ...
+                        </div>
+                    </div>
+                )}
+                {isDeleting && (
+                    <div className="isu-detail-row">
+                        <div className="alert alert-info" role="alert">
+                            Deleting your issue ...
+                        </div>
+                    </div>
+                )}
+                {(error || updateError || deleteError) && (
+                    <div className="isu-detail-row">
+                        <div className="alert alert-danger" role="alert">
+                            Unable to perform the operation. Please try again.
+                        </div>
+                    </div>
+                )}
             </div>
         </Modal>
     );
