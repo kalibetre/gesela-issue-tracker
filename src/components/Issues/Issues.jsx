4e3b773a43e808b65bc0b3ec99688b816ba3e0fd
kalibetre <kalbetre@icloud.com>
2023-05-18 21:03:50 +0300

    add a message and action card when there are no issues

--- src/components/Issues/Issues.jsx
+++ src/components/Issues/Issues.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import { useGetIssuesQuery } from '../../api/issueApi';
 import { groupByAttribute } from '../../utils/utils';
+import Button from '../Button/Button';
 import '../Common/Cards.css';
+import EditIssueModal from '../EditIssueModal/EditIssueModal';
 import IssueCard from '../IssueCard/IssueCard';
 import IssueDetail from '../IssueDetail/IssueDetail';
+import Logo from '../Logo/Logo';
 import StatusMessage from '../StatusMessage/StatusMessage';
 import Workspace from '../Workspace/Workspace';
 import './Issues.css';
@@ -11,6 +14,8 @@ import './Issues.css';
 const Issues = (props) => {
     const { data: issues, isLoading, isError } = useGetIssuesQuery();
     const [selectedIssue, setSelectedIssue] = useState(null);
+    const [newIssueModalOpen, setNewIssueModalOpen] = useState(false);
+
     const { groupBy, filter } = props;
     let groupedIssues = null;
     const ALL = 'ALL';
@@ -25,6 +30,40 @@ const Issues = (props) => {
         Object.keys(groupedIssues).forEach((group) => {
             groupedIssues[group] = groupedIssues[group].filter(filter);
         });
+    }
+
+    if (issues && issues.length === 0) {
+        return (
+            <Workspace title={props.title}>
+                {newIssueModalOpen && (
+                    <EditIssueModal
+                        handleClose={() => setNewIssueModalOpen(false)}
+                    />
+                )}
+                <div className="card-msg-container ">
+                    <div className="card-msg">
+                        <div className="card-msg-header">
+                            <Logo />
+                        </div>
+                        <div className="card-msg-content">
+                            <h2 className="card-msg-title">No issues found</h2>
+                            <p className="card-msg-text">
+                                There are no issues to display. Please create
+                                one to get started!
+                            </p>
+                            <div className="card-msg-action">
+                                <Button
+                                    className="btn btn-primary"
+                                    onClick={() => setNewIssueModalOpen(true)}
+                                >
+                                    Create New Issue
+                                </Button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Workspace>
+        );
     }
 
     return (
