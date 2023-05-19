d7a215ac287f4003f796757854e09ff34d4c40a4
kalibetre <kalbetre@icloud.com>
2023-05-19 21:42:55 +0300

    implement role assignment

--- src/components/AssignIssueModal/AssignIssueModal.jsx
+++ src/components/AssignIssueModal/AssignIssueModal.jsx
@@ -1,31 +1,57 @@
 import React, { useState } from 'react';
+import { useGetDepartmentsQuery } from '../../api/departmentApi';
 import { useGetEmployeesQuery } from '../../api/employeeApi';
 import { useUpdateIssueMutation } from '../../api/issueApi';
 import { Option, Select } from '../InputControls/InputControls';
+
 import Modal from '../Modal/Modal';
 
 const AssignIssueModal = (props) => {
+    const [selectedDeptUuid, setSelectedDeptUuid] = useState('');
+    const [selectedEmpUuid, setSelectedEmplUuid] = useState(null);
+    const [handlers, setHandlers] = useState([]);
+
+    const { data: departments, isLoading: loadingDepts } =
+        useGetDepartmentsQuery();
+
     const {
         data: employees,
         isLoading: loadingEmployees,
         isError: employeeLoadError,
     } = useGetEmployeesQuery();
-    const [selectedEmployee, setSelectedEmployee] = useState(null);
 
     const [updateIssue, { isLoading, isError }] = useUpdateIssueMutation();
     const [error, setError] = useState(null);
 
-    const handlers = employees
-        ? employees.filter((emp) => emp.role === 'ISSUE_HANDLER')
-        : [];
+    const handleDeptSelection = (e) => {
+        setSelectedDeptUuid((_) => {
+            if (employees) {
+                setHandlers((_) => {
+                    let handlerEmps = employees.filter(
+                        (emp) =>
+                            emp.role === 'ISSUE_HANDLER' &&
+                            emp.department.uuid === e.target.value
+                    );
+                    if (handlerEmps.length > 0)
+                        setSelectedEmplUuid(handlerEmps[0].uuid);
+                    else setSelectedEmplUuid(null);
+                    return handlerEmps;
+                });
+            }
+            return e.target.value;
+        });
+    };
 
     const handleSave = async (e) => {
         e.preventDefault();
         try {
-            const handler = selectedEmployee ?? employees[0];
+            if (!selectedEmpUuid) {
+                setError('Please select a handler for the issue');
+                return;
+            }
             await updateIssue({
                 uuid: props.issue.uuid,
-                handlerId: handler.uuid,
+                handlerId: selectedEmpUuid,
             }).unwrap();
             props.handleClose();
         } catch (error) {
@@ -51,6 +77,7 @@ const AssignIssueModal = (props) => {
                             isLoading ||
                             loadingEmployees ||
                             employeeLoadError ||
+                            loadingDepts ||
                             handlers.length <= 0
                         }
                         onClick={(e) => handleSave(e)}
@@ -61,16 +88,35 @@ const AssignIssueModal = (props) => {
             }
         >
             <div className="ap-new-issue-form">
-                {true && (
+                {departments && (
+                    <Select
+                        id="department"
+                        label="Department"
+                        value={selectedDeptUuid}
+                        onChange={handleDeptSelection}
+                        disabled={isLoading || loadingDepts}
+                    >
+                        {departments &&
+                            departments.map((department) => (
+                                <Option
+                                    value={department.uuid}
+                                    key={department.uuid}
+                                >
+                                    {department.name}
+                                </Option>
+                            ))}
+                    </Select>
+                )}
+                {selectedEmpUuid && (
                     <Select
                         id="status"
-                        label="Issue Status"
-                        value={selectedEmployee}
-                        onChange={(e) => setSelectedEmployee(e.target.value)}
-                        disabled={isLoading}
+                        label="Issue Handler"
+                        value={selectedEmpUuid}
+                        onChange={(e) => setSelectedEmplUuid(e.target.value)}
+                        disabled={isLoading || loadingDepts}
                     >
                         {handlers.map((employee) => (
-                            <Option value={employee} key={employee.uuid}>
+                            <Option value={employee.uuid} key={employee.uuid}>
                                 {employee.name}
                             </Option>
                         ))}
