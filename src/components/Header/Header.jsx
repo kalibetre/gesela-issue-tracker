d9634952a9974d436c70ea387d01bffec046e26c
kalibetre <kalbetre@icloud.com>
2023-05-20 13:24:38 +0300

    display role pill on the header for all except customers

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { useGetProfileQuery } from '../../api/userApi';
 import Avatar from '../Avatar/Avatar';
 import './Header.css';
 
 const Header = () => {
+    const { data: currentUser } = useGetProfileQuery();
+
     return (
         <header className="hd-container">
+            {currentUser && currentUser.role !== 'DEFAULT' && (
+                <div className="hd-role">
+                    {currentUser.role.replace('_', ' ')}
+                </div>
+            )}
             <div className="hd-acc">
                 <Avatar />
             </div>
