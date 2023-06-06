52e990902701e27eb6c25117977de9a6269055c3
kalibetre <kalbetre@icloud.com>
2023-06-06 21:54:21 +0300

    use query to load and display notifications as table

--- src/components/Notifications/Notifications.jsx
+++ src/components/Notifications/Notifications.jsx
@@ -1,13 +1,20 @@
 import React, { useState } from 'react';
+import { useGetNotificationsQuery } from '../../api/userApi';
+import { getFormattedDateTime } from '../../utils/utils';
+import MessageCard from '../MessageCard/MessageCard';
 import NotificationDetail from '../NotificationDetail/NotificationDetail';
+import StatusMessage from '../StatusMessage/StatusMessage';
 import Table from '../Table/Table';
 import Workspace from '../Workspace/Workspace';
 import './Notifications.css';
-import MessageCard from '../MessageCard/MessageCard';
 
 const Notifications = (props) => {
+    const {
+        data: notifications,
+        isLoading,
+        isError,
+    } = useGetNotificationsQuery();
     const [selectedNotification, setSelectedNotification] = useState(null);
-    const { notifications } = props;
 
     const handleRowClick = (notification) => {
         setSelectedNotification(notification);
@@ -17,7 +24,7 @@ const Notifications = (props) => {
         setSelectedNotification(null);
     };
 
-    if (!notifications.length) {
+    if (!isLoading && !isError && !notifications.length) {
         return (
             <Workspace title="Notifications">
                 <MessageCard
@@ -28,6 +35,16 @@ const Notifications = (props) => {
         );
     }
 
+    const normalizeData = (notification) => {
+        return {
+            uuid: notification.uuid,
+            date: getFormattedDateTime(notification.date),
+            from: notification.fromUser.name,
+            message: notification.message,
+            isSeen: notification.seen,
+        };
+    };
+
     return (
         <Workspace title="Notifications">
             {selectedNotification && (
@@ -36,14 +53,21 @@ const Notifications = (props) => {
                     handleClose={handleClose}
                 />
             )}
-            <div className="notifications">
-                <Table
-                    data={notifications}
-                    handleRowClick={handleRowClick}
-                    ignoreAttributes={{ id: 0, isRead: 4 }}
-                    highlightAttr="isRead"
-                />
-            </div>
+            {isLoading ? (
+                <StatusMessage loading title="Loading notifications ..." />
+            ) : isError ? (
+                <StatusMessage error title="Error notifications ..." />
+            ) : (
+                <div className="notifications">
+                    <Table
+                        data={notifications.map(normalizeData)}
+                        handleRowClick={handleRowClick}
+                        ignoreAttributes={{ uuid: 0, isSeen: 4 }}
+                        highlightAttr="isSeen"
+                        sortBy={(a, b) => new Date(b.date) - new Date(a.date)}
+                    />
+                </div>
+            )}
         </Workspace>
     );
 };
