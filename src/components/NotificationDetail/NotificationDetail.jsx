ffe55ef9d72c75bff62d4ac3dd0ad0f8761c872c
kalibetre <kalbetre@icloud.com>
2023-06-06 21:53:01 +0300

    set notification as read when user opens it

--- src/components/NotificationDetail/NotificationDetail.jsx
+++ src/components/NotificationDetail/NotificationDetail.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSetNotificationAsReadMutation } from '../../api/userApi';
 import Modal from '../Modal/Modal';
 import './NotificationDetail.css';
 
 const NotificationDetail = (props) => {
     const { notification } = props;
+    const [setNotificationsAsRead, { isLoading, isError }] =
+        useSetNotificationAsReadMutation();
+
+    useEffect(() => {
+        if (!notification.isSeen) setNotificationsAsRead(notification.uuid);
+    }, [notification, setNotificationsAsRead]);
+
     return (
         <Modal
             title="Notification Detail"
@@ -19,24 +27,32 @@ const NotificationDetail = (props) => {
                 </>
             }
         >
-            <div className="noti-detail">
-                <div className="noti-detail-row">
-                    <p className="noti-detail-label">From</p>
-                    <h3 className="noti-detail-noti-title">
-                        {notification.user}
-                    </h3>
+            {isLoading ? (
+                <>Loading please wait...</>
+            ) : isError ? (
+                <>Error getting notification details please try again later.</>
+            ) : (
+                <div className="noti-detail">
+                    <div className="noti-detail-row">
+                        <p className="noti-detail-label">From</p>
+                        <h3 className="noti-detail-noti-title">
+                            {notification.from}
+                        </h3>
+                    </div>
+                    <div className="noti-detail-row">
+                        <p className="noti-detail-label">Date</p>
+                        <h3 className="noti-detail-noti-title">
+                            {notification.date}
+                        </h3>
+                    </div>
+                    <div className="noti-detail-row">
+                        <p className="noti-detail-label">Message</p>
+                        <p className="noti-detail-desc">
+                            {notification.message}
+                        </p>
+                    </div>
                 </div>
-                <div className="noti-detail-row">
-                    <p className="noti-detail-label">Date</p>
-                    <h3 className="noti-detail-noti-title">
-                        {notification.date}
-                    </h3>
-                </div>
-                <div className="noti-detail-row">
-                    <p className="noti-detail-label">Message</p>
-                    <p className="noti-detail-desc">{notification.message}</p>
-                </div>
-            </div>
+            )}
         </Modal>
     );
 };
