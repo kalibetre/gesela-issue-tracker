187b20b29fcbbc9c2dc158de31cb4c2271815093
kalibetre <kalbetre@icloud.com>
2023-06-07 00:31:08 +0300

    add ReportCard component to display a single department stat

---
+++ src/components/ReportCard/ReportCard.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import PieChart, { CHART_COLORS } from '../PieChart/PieChart';
+import './ReportCard.css';
+
+const ReportCard = (props) => {
+    return (
+        <div className="report-card">
+            <div className="report-container">
+                <div className="report-stats">
+                    <div className="report-subtitle">Department</div>
+                    <div className="report-title">Administration</div>
+                    <div className="report-data-container">
+                        <div className="report-data">
+                            {Object.entries(props.data).map(([key, value]) => (
+                                <>
+                                    <div className="report-data-value">
+                                        {value}
+                                    </div>
+                                    <div className="report-data-key">
+                                        <span
+                                            style={{
+                                                backgroundColor:
+                                                    CHART_COLORS[key],
+                                            }}
+                                        ></span>
+                                        {key}
+                                    </div>
+                                </>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+                <div className="report-chart">
+                    <PieChart data={props.data} />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ReportCard;
