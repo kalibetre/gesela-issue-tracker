d19ba30602261075cc323679d715ee6835d4b2b9
kalibetre <kalbetre@icloud.com>
2023-06-14 21:39:51 +0300

    add search input to workspace

--- src/components/Workspace/Workspace.jsx
+++ src/components/Workspace/Workspace.jsx
@@ -6,6 +6,14 @@ const Workspace = (props) => {
         <section className="wks-main-container">
             <div className="wks-header">
                 <h2 className="wks-title">{props.title}</h2>
+                <div className="wks-search">
+                    <div className="wks-search-input-container">
+                        <input
+                            className="wks-search-input"
+                            placeholder="search"
+                        />
+                    </div>
+                </div>
             </div>
             <main className="wks-main">{props.children}</main>
         </section>
